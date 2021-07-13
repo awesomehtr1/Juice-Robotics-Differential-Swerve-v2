a8be816d01bddd88bc881f8ce7e93fb9fa06d5f5
Benjamin Colby <bcolby@gmail.com>
2021-07-13 11:47:43 -0700

    initial commit, implement robot class and subsystem interface

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,4 +1,60 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Hardware;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.MecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.Subsystem;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Robot {
+    public List<Subsystem> subsystems;
+    public List<TeleopManager> teleopManagers;
+    public HardwareMap hardwareMap;
+    public MecanumDrive mecanumDrive;
+    public SampleMecanumDrive drive;
+
+    public Gamepad gamepad1, gamepad2;
+
+    public Robot(HardwareMap hardwareMap){
+        this.hardwareMap = hardwareMap;
+        drive = new SampleMecanumDrive(hardwareMap);
+        mecanumDrive = new MecanumDrive(this);
+
+        subsystems = new ArrayList<>();
+        Collections.addAll(
+                subsystems,
+                mecanumDrive
+        );
+
+        teleopManagers = new ArrayList<>();
+    }
+
+    public Robot(HardwareMap hardwareMap, Gamepad gamepad1, Gamepad gamepad2){
+        this.hardwareMap = hardwareMap;
+        drive = new SampleMecanumDrive(hardwareMap);
+        mecanumDrive = new MecanumDrive(this);
+
+        subsystems = new ArrayList<>();
+        Collections.addAll(
+                subsystems,
+                mecanumDrive
+        );
+
+        this.gamepad1 = gamepad1;
+        this.gamepad2 = gamepad2;
+    }
+
+    public void update(){
+        for (Subsystem s: subsystems)
+            s.update();
+        for (TeleopManager t: teleopManagers)
+            t.update();
+    }
 }
